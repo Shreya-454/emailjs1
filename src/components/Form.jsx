fd93ec0bbbad9eb96e6b8f54ced0dcafcbfe62b4
Shreya-454 <bshreya932@GMAIL.COM>
2024-04-12 09:24:50 +0530

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_y3mxtot", "template_sgngerd", form.current, {
+        publicKey: "GJQJ5xe8a3nmu8JrK",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
+  return (
+    <div className=" flex justify-center mx-auto w-[500px] container">
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className=" w-full mx-auto"
+      >
+        <div className="w-full">
+          <div className="w-full">
+            <label>Name</label>
+          </div>
+          <input type="text" name="user_name" className="border border-solid border-black h-[50px] w-full" />
+        </div>
+        <div className="w-full">
+          <div className="w-full">
+            <label>Email</label>
+          </div>
+          <input type="email" name="user_email" className="border border-solid border-black h-[50px] w-full" />
+        </div>
+        <div className="w-full">
+          <div>
+            <label>Message</label>
+          </div>
+          <textarea name="message" className="border border-solid border-black !h-[80px] w-full resize-none"/>
+        </div>
+        <div className="w-full">
+          <input type="submit" value="Send" className="bg-black text-white rounded-md px-5 py-3" />
+        </div>
+      </form>
+    </div>
+  );
+};
